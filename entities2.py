145c531378b180b5a93da09e5a64c0cd724ca960
Gaspard Chupin <gaspard.chupin@etu.minesparis.psl.eu>
2026-01-30 20:23:45 +0100

    Changement, pause repas

--- entities2.py
+++ entities2.py
@@ -8,14 +8,40 @@ class Animal:
     age: int
     energy: int
 
-    def eat(self, )
-        
-
+    def eat(self, energy_gain):
+        self.energy = self.energy + energy_gain
+    
 
 
 @dataclass
 class Sheep(Animal):
-    def
+    
+    def deplacement(self, grille, gsize, gain):
+        i, j = self.position
+        if j<gsize-1 and grille.grass[i][j+1].state == 1 and grille.animals[i][j+1] == 0:
+            self.position = (i,j+1)
+            self.eat(gain)
+        elif j>0 and grille.grass[i][j-1].state == 1 and grille.animals[i][j-1] == 0:
+            self.position = (i, j-1)
+            self.eat(gain)
+        elif i>0 and grille.grass[i-1][j].state == 1 and grille.animals[i][j+1] == 0:
+            self.position = (i-1,j)
+            self.eat(gain)
+        elif i<gsize-1 and grille.grass[i+1][j].state == 1 and grille.animals[i][j+1] == 0:
+            self.position = (i+1,j)
+            self.eat(gain)
+        else: 
+            nextposition = []
+            if j<gsize-1 and grille.animals[i][j+1] == 0 :
+                nextposition.append((i,j+1))
+            elif j>0 and grille.animals[i][j+1] == 0 :
+                nextposition.append((i,j-1))
+            elif i>0 and grille.animals[i][j+1] == 0 :
+                nextposition.append((i-1,j))
+            elif i<gsize-1 and grille.animals[i][j+1] == 0 :
+                nextposition.append((i-1,j))
+            if nextposition != []:
+                self.position = rd.choice(nextposition)
 
 
 
@@ -26,23 +52,24 @@ class Wolf(Animal):
 
 
 @dataclass
-class Grass:
-    state: int
-    time: int
+class grass:                                        # Définition de la classe grass (herbe)
+    state: int                                          # Propriété state (état). 0 pour un sol vide, 1 pour de l'herbe
+    time: int                                           # Propriété time (temps), donne le temps de repousse
 
-    def new_grass(self, proba):
-        if self.state == 0:
-            if rd.random(0, 1) <= proba:
-                self.state = 1
-                self.time = 0
+    def new_grass(self, proba):                         # Fonction d'apparition de l'herbe aléatoirement sur un sol vide
+        if self.state == 0:                                 # Si la case ne contient pas d'herbe
+            if rd.random(0, 1) <= proba:                        # On la compare à notre taux d'apparition spontané
+                self.state = 1                                  # Si on remplit la condition, on fait apparaître l'herbe
+                self.time = 0                                   # On réinitialise le temps de repousse de l'herbe
     
-    def regrowth(self, time):
-        if self.state == 0:
-            self.time += 1
-            if self.time == time
-                self.state = 1
+    def regrowth(self, time):                           # Fonction de repousse
+        if self.state == 0:                                 # Si la case ne contient pas d'herbe
+            self.time += 1                                      # On incrémente le temps de repousse
+            if self.time == time:                                   # Si on atteint la durée de repousse
+                self.state = 1                                      # On fait apparaître de l'herbe
+                self.time = 0                                       # On réinitialise le temps de repousse
     
-    def eaten_grass(self):
-        if isinstance(self, Sheep) == True:
-            self.state = 0
-            self.time = -1
+    def eaten_grass(self):                              # Fonction de détection de l'herbe mangée
+        if isinstance(self, Sheep) == True:                 # Si un mouton est sur la case (c'est qu'il a mangé l'herbe)
+            self.state = 0                                      # On enlève l'herbe
+            self.time = -1                                      # On passe son temps de repousse à -1 (cette fonction est effectuée avant new_grass et regrowth)
--- main_copy.py
+++ main_copy.py
@@ -1,4 +1,4 @@
-from entities2 import Sheep, Wolves, Grass
+from entities2 import Animal, Sheep, Wolf, Grass
 import random as rd
 from grid_copy import Grid
 import pyxel 
@@ -13,57 +13,66 @@ def endgame():
 
 
 def game(MAX_TURNS=500):
-    # DÉROULÉ DU TOUR
-    GRID_SIZE = 30
-    SHEEP_INITIAL_ENERGY = 20 
+    
+    # Initialisation des paramètres
+    grid_size = 30
+    sheep_initial_energy = 20
+    wolf_initial_energy = 40
     SHEEP_ENERGY_LOSS_PER_TURN= 1
     REPRODUCTION_ENERGY_COST = 20
     SHEEP_REPRODUCTION_THRESHOLD = 50
-    SHEEP_ENERGY_FROM_GRASS = 15
+    sheep_energy_from_grass = 15
     grass_regrowth_time = 7
     grass_growth_probability = 0.08
     SHEEP_MAX_AGE = 50
     WOLF_MAX_AGE = 40
-    NB_SHEEP_INIT = 50
-    NB_WOLVES_INIT = 10
-    NB_GRASS_INIT = int(0.3*GRID_SIZE**2)
-    WOLF_INITIAL_ENERGY = 40
+    nb_sheep_init = 50
+    nb_wolves_init = 10
+    grass_proportion_init = 30
+    nb_grass_init = int(grass_proportion_init/100*grid_size**2)
     
-    ## initialisation
-    grid = GRID()
-    def init_sheep_wolves_grass(n_sheep,n_wolves,n_grass,GRID):
-        if n_sheep + n_wolves > GRID_SIZE**2:
-            return False
-        else:
-            T = [[(i,j) for j in range (GRID_SIZE)] for i in range(GRID_SIZE)]
-            L1 = []
-            for ligne in T:
-                for elt in ligne:
-                    L1.append(elt)
-            L2 = rd.sample(L1,n_sheep+n_wolves)
-            Ls = rd.sample(L1,n_sheep)
-            for i,j in Ls:
-                GRID.ELT[i][j] = Sheep((i,j))
-            for i,j in L2:
-                if (i,j) not in Ls:
-                    GRID.ELT[i][j] = Wolves((i,j))
-            Lg = rd.sample(L1,n_grass)
-            for i,j in L1:
-                GRID.ELT[i][j] = Grass(0, 0, grass_regrowth_time, grass_growth_probability)
-            for i,j in Lg:
-                GRID.ELT[i][j].STATE = 1 
+    ## Initialisation de la grille
+    grass_grid = [[Grass(0, 0) for i in range(grid_size)]for j in range(grid_size)]     # Calque pour le sol/l'herbe
+    animal_grid = [[Animal() for i in range(grid_size)]for j in range(grid_size)]       # Calque pour les animaux
     
-    init_sheep_wolves_grass(NB_SHEEP_INIT,NB_WOLVES_INIT,NB_GRASS_INIT,grid)
+    if nb_sheep_init + nb_wolves_init > grid_size**2:                                   # Le nombre d'animaux à l'état initial ne peut pas dépasser la taille de la grille
+        return False
+    else:
+        Tempcoord = [[(i,j) for j in range(grid_size)] for i in range(grid_size)]       # Calque temporaire donnant les coordonnées des cases de la grille
+        Tempnumb = []                                                                   # Calque temporaire donnant le numéro des cases de la grille
+        for ligne in Tempcoord :
+            for nb in ligne:
+                Tempnumb.append(nb)                                                             # la boucle for sert juste à donner un numéro à chaque case
+
+        Whereanimals = rd.sample(Tempnumb, nb_sheep_init + nb_wolves_init)              # On attribue de façon aléatoire une position sur la grille aux animaux (peu importe le type d'animal)
+        Wheresheep = rd.sample(Whereanimals, nb_sheep_init)                             # Parmi ces numéros, on précise lesquels sont pour les moutons
+        for i,j in Wheresheep:                                                          # On place les moutons sur les cases qui leur ont été attribuées
+            animal_grid[i][j] = Sheep(position=(i,j), age=0, energy=sheep_initial_energy)
+        for i,j in Whereanimals:                                                        # On met les loups sur les cases restantes
+            if (i,j) not in Wheresheep:
+                animal_grid[i][j] = Wolf(position=(i,j), age=0, energy=wolf_initial_energy)
+        
+        Wheregrass = rd.sample(Tempnumb, nb_grass_init)                                 # On attribue de façon aléatoire des cases pour l'herbe
+        for i,j in Tempnumb:                                                            # On place du sol vide avec un état de repousse aléatoire
+            temps = rd.random(0,6)
+            grass_grid[i][j] = Grass(0, temps)
+        for i,j in Wheregrass:                                                          # On place de l'herbe, en pensant à mettre son état de repousse à 0
+            grass_grid[i][j].state = 1
+            grass_grid[i][j].time = 0
+
+    grid = Grid(grid_size, grass_grid, animal_grid)                                     # Maintenant que nos calques sont prêts, on fabrique l'objet Grid les comprenant
+
+
     pyxel.init(GRID_SIZE,GRID_SIZE,title = "Ecosystème",fps = 10)
     number_of_animals = 1
     turn_number = 0
     while (turn_number < MAX_TURNS) and (number_of_animals>0):
         turn_number += 1
-        tour(grid, MAX_TURNS, turn_number)
+        tour(grid, MAX_TURNS, turn_number, grid_size, sheep_energy_from_grass, wolf_energy_from_sheep)
         number_of_animals = 0
         for ligne in grid.ELT:
             for elt in ligne:
-                if isinstance(elt, Sheep) or isinstance(elt, Wolves):
+                if isinstance(elt, Sheep) or isinstance(elt, Wolf):
                     number_of_animals += 1
         pyxel.run(tour(grid),draw(grid,GRID_SIZE))
     
@@ -71,12 +80,15 @@ def game(MAX_TURNS=500):
     endgame()
 
 
-def tour(grid, nbmaxtours, touractuel):
+
+
+
+def tour(grid, nbmaxtours, touractuel, size, sheepgain, wolfgain):
     time.sleep(1)
     # PHASE 1 : INCREMENT DE L'AGE DES ANIMAUX
-    for ligne in grid.ELT:
+    for ligne in grid.animals:
         for elt in ligne:
-            if isinstance(elt, Sheep) or isinstance(elt, Wolves):
+            if isinstance(elt, Sheep) or isinstance(elt, Wolf):
                 elt.age()
                 
                                     
@@ -100,8 +112,8 @@ def tour(grid, nbmaxtours, touractuel):
     for ligne in grid.grass:
         for case in ligne:
             case.eaten_grass()                                               # On regarde si l'herbe a été mangée
-            case.new_grass(grass_growth_probability)                                                 # On regarde si l'herbe peut apparaitre aléatoirement
-            case.regrowth(grass_regrowth_time)                                                  # On fait repousser l'herbe en fonction de son temps de repousse
+            case.new_grass(grass_growth_probability)                         # On regarde si l'herbe peut apparaitre aléatoirement
+            case.regrowth(grass_regrowth_time)                               # On fait repousser l'herbe en fonction de son temps de repousse
 
 
     # PHASE 3 : MOUTONS
@@ -111,7 +123,7 @@ def tour(grid, nbmaxtours, touractuel):
             # repérage des moutons dans la grille
             if isinstance(grid.ELT[k][l],Sheep):
                 current_sheep = grid.ELT[k][l]
-                current_sheep.deplacement(current_sheep,grid)   # déplace et alimente les moutons
+                current_sheep.deplacement(current_sheep,grid, size, sheepgain)   # déplace et alimente les moutons
                 current_sheep.energy(current_sheep)
 
     # PHASE 4 : LOUPS
