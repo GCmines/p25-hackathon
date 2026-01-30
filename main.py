f5b4b09a664c31f54c1adcfce1770a92ba16b2d4
Gaspard Chupin <gaspard.chupin@etu.minesparis.psl.eu>
2026-01-30 12:31:53 +0100

    Mise à jour main

--- main.py
+++ main.py
@@ -12,14 +12,24 @@ from grid import GRID
 
 
 # DÉROULÉ DU TOUR
-    # INCREMENT DE L'AGE DES ANIMAUX
+# PHASE 1 : INCREMENT DE L'AGE DES ANIMAUX
 
 
 
 
-    # MISE A JOUR DE L'HERBE
+# PHASE 2 : MISE A JOUR DE L'HERBE
 for x in range(0, GRID_SIZE - 1):                                   # On parcourt les x et y
     for y in range(0, GRID_SIZE - 1):
         Grass.eaten_grass(x, y)                                     # On regarde si l'herbe a été mangée
         Grass.new_grass(x, y)                                       # On regarde si l'herbe peut apparaitre aléatoirement
-        Grass.regrowth(x, y)
+        Grass.regrowth(x, y)                                        # On fait repousser l'herbe en fonction de son temps de repousse
+
+# PHASE 3 : MOUTONS
+# PHASE 4 : LOUPS
+# PHASE 5 : Vérification des morts???
+# PHASE 6 : REPRODUCTION
+# PHASE 7 : AFFICHAGE DE L'ÉTAT OBTENU
+
+
+# PHASE 8 : VÉRIFICATION DES CONDITIONS D'ARRÊT
+
