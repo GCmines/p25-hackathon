bc8abbc2aad79ef6b0ebd0f9c13a38c8e99dd6cb
Astrid Allegretti <astrid.allegretti@etu.minesparis.psl.eu>
2026-01-30 12:30:26 +0100

    Modification grille avec 0 et Sheep

--- entities.py
+++ entities.py
@@ -13,32 +13,32 @@ class Sheep:
     
     def deplacement(self,GRID):
         i,j = self.POSITION
-        if j<GRID_SIZE-1 and GRID.GRASS[i][j+1].STATE == 1 and GRID.ELT[i][j+1] == None:
+        if j<GRID_SIZE-1 and GRID.GRASS[i][j+1].STATE == 1 and GRID.ELT[i][j+1] == 0:
             self.POSITION = (i,j+1)
             self.eat()
-        elif j>0 and GRID.GRASS[i][j-1].STATE == 1 and GRID.ELT[i][j-1] == None:
+        elif j>0 and GRID.GRASS[i][j-1].STATE == 1 and GRID.ELT[i][j-1] == 0:
             self.POSITION = (i, j-1)
             self.eat()
-        elif i>0 and GRID.GRASS[i-1][j].STATE == 1 and GRID.ELT[i][j+1] == None:
+        elif i>0 and GRID.GRASS[i-1][j].STATE == 1 and GRID.ELT[i][j+1] == 0:
             self.POSITION = (i-1,j)
             self.eat()
-        elif i<GRID_SIZE-1 and GRID.GRASS[i+1][j].STATE == 1 and GRID.ELT[i][j+1] == None:
+        elif i<GRID_SIZE-1 and GRID.GRASS[i+1][j].STATE == 1 and GRID.ELT[i][j+1] == 0:
             self.POSITION = (i+1,j)
             self.eat()
         else: 
             position = []
-            if j<GRID_SIZE-1 and GRID.ELT[i][j+1] == None :
+            if j<GRID_SIZE-1 and GRID.ELT[i][j+1] == 0 :
                 position.append((i,j+1))
-            elif j>0 and GRID.ELT[i][j+1] == None :
+            elif j>0 and GRID.ELT[i][j+1] ==0 :
                 position.append((i,j-1))
-            elif i>0 and GRID.ELT[i][j+1] == None :
+            elif i>0 and GRID.ELT[i][j+1] == 0 :
                 position.append((i-1,j))
-            elif i<GRID_SIZE-1 and GRID.ELT[i][j+1] == None :
+            elif i<GRID_SIZE-1 and GRID.ELT[i][j+1] == 0 :
                 position.append((i-1,j))
             if position != []:
                 self.POSITION = rd.choice(position)
         sheep = GRID.ELT[i,j]
-        GRID.ELT[i,j] = None
+        GRID.ELT[i,j] = 0
         GRID.ELT[self.POSITION[0]][self.POSITION[1]]
         
     def eat(self):
@@ -50,19 +50,20 @@ class Sheep:
             self.AGE +=1
             self.ENERGY = self.ENERGY -SHEEP_ENERGY_LOSS_PER_TURN
         elif self.AGE>=AGE_LIMITE :
-            self.ALIVE = False
+            i,j = self.POSITION
+            GRID.ELT[i][j] = 0
     
     def reproduction(self,GRID):
         if self.ENERGY>SHEEP_REPRODUCTION_THRESHOLD:
             i,j = self.POSITION
             self.ENERGY = self.ENERGY - REPRODUCTION_ENERGY_COST
-            if (i+1<GRID_SIZE) and not(isinstance(GRID.ELT[i+1][j],Sheep) or isinstance(GRID.ELT[i+1][j],Wolves)):
+            if (i+1<GRID_SIZE) and (GRID.ELT[i+1][j]==0):
                 GRID.ELT[i+1][j] = Sheep((i+1,j))
-            elif (i-1>=0) and not(isinstance(GRID.ELT[i-1][j],Sheep) or isinstance(GRID.ELT[i-1][j],Wolves)):
+            elif (i-1>=0) and (GRID.ELT[i-1][j]==0):
                 GRID.ELT[i-1][j] = Sheep((i-1,j))
-            elif (j+1<GRID_SIZE) and not(isinstance(GRID.ELT[i][j+1],Sheep) or isinstance(GRID.ELT[i][j+1],Wolves)):            
+            elif (j+1<GRID_SIZE) and (GRID.ELT[i][j+1]==0):            
                 GRID.ELT[i][j+1] = Sheep((i,j+1))
-            elif (j-1>0) and not(isinstance(GRID.ELT[i][j-1],Sheep) or isinstance(GRID.ELT[i][j-1],Wolves)):            
+            elif (j-1>0) and (GRID.ELT[i][j-1]==0):            
                 GRID.ELT[i][j-1] = Sheep((i,j-1))
             
     
